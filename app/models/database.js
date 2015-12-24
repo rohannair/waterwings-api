27f60eae6c5dde200c8df24d2f9cf659befa88f7
Rohan Nair <rnair@terapeak.com>
2015-12-24 18:22:13 -0500

    Adding in instructions for setting up and starting dbs [skip ci]

--- app/models/database.js
+++ app/models/database.js
@@ -1,13 +1,14 @@
 import pg from 'pg';
 import Sequelize from 'sequelize';
 
-// Schema
+// Import model factories
 import Users from './Users.js';
 import Companies from './Companies.js';
 import Products from './Products.js';
 import ProductTypes from './ProductTypes.js';
 import Packages from './Packages.js';
 
+// Set up database
 const sequelize   = new Sequelize('waterwings', 'root', 'password', {
   host: 'localhost',
   dialect: 'postgres',
@@ -19,33 +20,16 @@ const sequelize   = new Sequelize('waterwings', 'root', 'password', {
   },
 });
 
+// Set models
 const User        = Users(sequelize);
 const Company     = Companies(sequelize);
 const Product     = Products(sequelize);
 const ProductType = ProductTypes(sequelize);
 const Package     = Packages(sequelize);
 
-let dummyUser = {
-  firstName  : 'Rohan',
-  lastName   : 'Nair',
-  email      : 'r@rohannair.ca',
-  phoneNumber: '6479707998'
-};
-
-let dummyCompany = {
-  name       : 'Quartermaster',
-  address1   : '28 Ted Rogers Way',
-  address2   : 'Apt 3404',
-  city       : 'Toronto',
-  province   : 'ON',
-  country    : 'CA',
-  phoneNumber: '6479707998'
-};
-
+// Sync models with Postgres
 Package.sync({force: true})
   .then(Company.sync({force:true}))
   .then(Product.sync({force:true}))
   .then(ProductType.sync({force:true}))
-  .then(User.sync({force:true}))
-  .then(() => User.create(dummyUser))
-  .then(() => Company.create(dummyCompany));
+  .then(User.sync({force:true}));
