6a046991b1811f1ff9753b19043eb59ac5d3fa85
Stosh Fabricius <stosh.fabricius@gmail.com>
2016-04-29 14:25:00 -0400

    Implment hashing functions into the login and user create routes

--- app/utils/encryption.js
+++ app/utils/encryption.js
@@ -1,14 +1,29 @@
 const bcrypt = require('bcrypt');
 
-// Async functon to encryt a password, given the password and a saltRound value
-// A good default for saltRounds is 10
-function encryptPassword(password, saltRounds) {
+// Async function to encrypt a password, given the password and a saltRound value
+function encryptPassword(password, saltRounds = 10) {
   return new Promise((resolve, reject) => {
     bcrypt.hash(password, saltRounds, (err, hash) => {
-      if(err !== null) return reject(err);
+      if (err !== undefined) return reject(err);
       resolve(hash);
     });
   });
 }
 
-module.exports = encryptPassword;
+// password is the password supplied by the user at the login screen
+// dbHash is the hashed password in the database
+// returns a boolean depending on the match
+function checkPassword(password, dbHash) {
+  console.log(arguments);
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, dbHash, (err, res) => {
+      if (err !== undefined) return resolve(false);
+      resolve(res);
+    });
+  })
+}
+
+module.exports = {
+  encryptPassword: encryptPassword,
+  checkPassword: checkPassword
+};
