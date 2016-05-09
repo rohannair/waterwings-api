493a8a32775ecb156274444d813b1d51db35dcb3
Rohan Nair <r@rohannair.ca>
2016-05-09 18:31:11 -0400

    Implementing uploadsController, allowing for image uploads

--- app/controllers/uploadsController.js
+++ app/controllers/uploadsController.js
@@ -1,31 +1,56 @@
+const chalk = require('chalk');
 const multipart = require('co-multipart');
 const fetch = require('node-fetch');
+const fs = require('fs');
+const R = require('ramda');
 
 const uploadsController = (function() {
+
+  // function to encode file data to base64 encoded string
+  function* base64_encode(file) {
+    return new Buffer(fs.readFileSync(file)).toString('base64');
+  }
+
   return {
     POST: function* () {
       try {
         const parts = yield* multipart(this);
-        let self = this;
+        const self = this;
 
-        // do stuff with the body parts
-        parts.files.forEach(function(file) {
-          console.log(file.fileName || file.filename)
-          // yield fetch('localhost:3002', {
-          //   method: 'POST',
-          //   body: file
-          // })
-          // .then(resp => resp.json)
-          // .then(resp => {
-          //   this.body = resp;
-          // });
-        });
+        const file = parts.files[0];
+        const fileName = file.filename
+          .split('.')
+          .map(str => str.toLowerCase())
+          .map(str => str.split(' ').join('_'))
+          .join('.');
 
-        // delete all the files when you're done
-        parts.dispose();
+        let base64File = yield base64_encode(file.path);
 
+        yield fetch('http://localhost:3002/upload', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: fileName,
+            file: base64File
+          })
+        })
+        .then(resp => {
+          if (!resp.ok) return self.throw(resp.statusText, resp.status);
+          return resp.json();
+        })
+        .then(resp => self.body = resp)
+        .then(() => parts.dispose());
+
+        this.status = 201;
       } catch (e) {
         console.log(chalk.red.bold(JSON.stringify(e, null, 4)));
+        this.status = e.status || 400;
+        this.body = {
+          message: e.message,
+          error: JSON.stringify(e)
+        }
       }
     }
   }
