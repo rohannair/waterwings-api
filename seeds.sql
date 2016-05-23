9c1d0cb66c09f6484970e91f9b72a7eb9f729c47
Rohan Nair <r@rohannair.ca>
2016-05-23 17:14:24 -0400

    Fixing seed file..

--- seeds.sql
+++ seeds.sql
@@ -160,85 +160,79 @@ INSERT INTO playbooks (id, name, description, company_id, doc) VALUES
   }'
 ), ('72ade965-ccc3-406b-b4d9-b3e233e6bf58', 'Lighthouse Toronto Mentor', 'Toronto Mentor', 'OeUMkxw',
   '{
-   "id":"72ade965-ccc3-406b-b4d9-b3e233e6bf58",
-   "name":"Lighthouse Toronto Mentor",
-   "description":"Toronto Mentor",
-   "company_id":"OeUMkxw",
-   "doc":{
-      "0":{
-         "body":"<p>Welcome to Lighthouse Labs - We are really excited to have your contribution helping us teach and grow the next generation of developer talent!.As part of your on-boarding, there are a few things to do done:</p>\n<ol>\n  <li>Fill out your user profile &amp; upload docs</li>\n  <li>Access team groups</li>\n  <li>Knowledge Center</li>\n  <li>See the schedule for your first day</li>\n</ol>\n<p>If you have any questions feel free to contact <a href=\"mailto:kate@lighthouselabs.ca\">kate@lighthouselabs.ca</a></p>",
-         "type":"intro",
-         "heading":"Hey JerKhurram, welcome aboard!",
-         "slide_number":0
-      },
-      "1":{
-         "body":{
-            "heading":"Fill out your Employee profile",
-            "options":{
-               "bio":true,
-               "name":true,
-               "twitter":false,
-               "facebook":false,
-               "linkedin":false,
-               "profile_image":true
-            }
-         },
-         "type":"bio",
-         "slide_number":1
-      },
-      "2":{
-         "body":"<p><a href=\"http://compass.lighthouselabs.ca/i/ykGpp112qqnbfk67hi5ro3wll\"><ins><strong>Compass Access</strong></ins></a></p>\n<p>Use the following URL to register and gain teacher access to the LMS, curriculum, students, etc. Please do not  will want to be checking what students are learning during lectures and in assignments prior to your shifts.</p>\n<p><strong>Email</strong></p>\n<p>You’ve been added to the web-teachers-toronto@lighthouselabs.ca email (google) group, which means you’ll be getting some emails from Compass (via instructors) and staff, and have access to our important gDocs.</p>\n<p><a href=\"https://docs.google.com/a/functionalimperative.com/spreadsheets/d/1BB4e_7Ih2lkmG9oD6vIhCgtPjq2rKiLOndPEcGMr8zE/edit\"><ins><strong>Schedule</strong></ins></a></p>\n<p>This is an important document, please bookmark it and check it weekly if not more regularly for changes.</p>\n<p>You’ve been added into the Teacher Info sheet, but please complete all your details in there at your earliest convenience. It will help us with scheduling going forward.</p>\n<p><strong>Slack</strong></p>\n<p>You have been sent an invitation to join the Toronto teacher channel. &nbsp;Use this to quickly communicate with your fellow LHL teachers and staff.</p>\n<p><strong>ClockTower</strong></p>\n<p>The on-boarding document explains what this is and why it is important to you. Your account has been created and you should have another email in your inbox with your credentials. Please send your invoices, as well as any concerns or questions re payment, to Landon at <a href=\"mailto:bookkeeper@lighthouselabs.ca\">bookkeeper@lighthouselabs.ca</a></p>",
-         "type":"intro",
-         "heading":"Your toolkit",
-         "slide_number":2
-      },
-      "3":{
-         "body":{
-            "desc":"We have got a lot of learning content to get you ready for your role. We recommend that you go through the following lesssons before your first day, to hit the ground running:",
-            "options":[
-               {
-                  "id":"nu4SERt8hyA",
-                  "type":"youtube",
-                  "name":"A Welcome from our Head Instructor Don!"
-               },
-               {
-                  "id":0,
-                  "type":"link",
-                  "name":"Lighthouse Labs Teacher Guide",
-                  "link":"https://docs.google.com/document/d/1K0vbFwOIAYzCyqfIYEQZWXsaeDpBKajq_E-3oIEnkYU/edit"
-               }
-            ]
-         },
-         "type":"knowledgectr",
-         "heading":"Knowledge Center",
-         "slide_number":3
-      },
-      "4":{
-         "body":{
-            "map":"<div><iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.1693571231563!2d-79.39719274890832!3d43.64464467901919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34d97247bbc3%3A0xbc1d36bccb4b45ba!2sLighthouse+Labs!5e0!3m2!1sen!2sca!4v1463603588516\" frameborder=\"0\" allowfullscreen></iframe><div><span class=\"fa fa-building\"></span> 46 Spadina Ave, Toronto, ON <a href=\"#\">Get Directions</a></div><div><span class=\"fa fa-user\"></span> Office Manager - <a href=\"#\">John Smith</a></div><div><span class=\"fa fa-envelope\"></span><a href=\"#\">J.Smith@lighthouselabs.ca</a></div><div><div class=\"uber-promo\"> Complimentary UBER Code <strong>Welcome2LighthouseLabs</strong></div></div></div>",
-            "agenda":[
-               {
-                  "desc":"Morning Class (2.5 hours)",
-                  "time":"9:00am - 10:30am"
-               },
-               {
-                  "desc":"Sinatra Apps (Long)",
-                  "time":"10:30am - End of Day"
-               }
-            ]
-         },
-         "type":"day1agenda",
-         "heading":"Your first day - February 29th, 2016",
-         "slide_number":4
-      }
-   }
-}'
+    "0":{
+       "body":"<p>Welcome to Lighthouse Labs - We are really excited to have your contribution helping us teach and grow the next generation of developer talent!.As part of your on-boarding, there are a few things to do done:</p>\n<ol>\n  <li>Fill out your user profile &amp; upload docs</li>\n  <li>Access team groups</li>\n  <li>Knowledge Center</li>\n  <li>See the schedule for your first day</li>\n</ol>\n<p>If you have any questions feel free to contact <a href=\"mailto:kate@lighthouselabs.ca\">kate@lighthouselabs.ca</a></p>",
+       "type":"intro",
+       "heading":"Hey JerKhurram, welcome aboard!",
+       "slide_number":0
+    },
+    "1":{
+       "body":{
+          "heading":"Fill out your Employee profile",
+          "options":{
+             "bio":true,
+             "name":true,
+             "twitter":false,
+             "facebook":false,
+             "linkedin":false,
+             "profile_image":true
+          }
+       },
+       "type":"bio",
+       "slide_number":1
+    },
+    "2":{
+       "body":"<p><a href=\"http://compass.lighthouselabs.ca/i/ykGpp112qqnbfk67hi5ro3wll\"><ins><strong>Compass Access</strong></ins></a></p>\n<p>Use the following URL to register and gain teacher access to the LMS, curriculum, students, etc. Please do not  will want to be checking what students are learning during lectures and in assignments prior to your shifts.</p>\n<p><strong>Email</strong></p>\n<p>You’ve been added to the web-teachers-toronto@lighthouselabs.ca email (google) group, which means you’ll be getting some emails from Compass (via instructors) and staff, and have access to our important gDocs.</p>\n<p><a href=\"https://docs.google.com/a/functionalimperative.com/spreadsheets/d/1BB4e_7Ih2lkmG9oD6vIhCgtPjq2rKiLOndPEcGMr8zE/edit\"><ins><strong>Schedule</strong></ins></a></p>\n<p>This is an important document, please bookmark it and check it weekly if not more regularly for changes.</p>\n<p>You’ve been added into the Teacher Info sheet, but please complete all your details in there at your earliest convenience. It will help us with scheduling going forward.</p>\n<p><strong>Slack</strong></p>\n<p>You have been sent an invitation to join the Toronto teacher channel. &nbsp;Use this to quickly communicate with your fellow LHL teachers and staff.</p>\n<p><strong>ClockTower</strong></p>\n<p>The on-boarding document explains what this is and why it is important to you. Your account has been created and you should have another email in your inbox with your credentials. Please send your invoices, as well as any concerns or questions re payment, to Landon at <a href=\"mailto:bookkeeper@lighthouselabs.ca\">bookkeeper@lighthouselabs.ca</a></p>",
+       "type":"intro",
+       "heading":"Your toolkit",
+       "slide_number":2
+    },
+    "3":{
+       "body":{
+          "desc":"We have got a lot of learning content to get you ready for your role. We recommend that you go through the following lesssons before your first day, to hit the ground running:",
+          "options":[
+             {
+                "id":"nu4SERt8hyA",
+                "type":"youtube",
+                "name":"A Welcome from our Head Instructor Don!"
+             },
+             {
+                "id":0,
+                "type":"link",
+                "name":"Lighthouse Labs Teacher Guide",
+                "link":"https://docs.google.com/document/d/1K0vbFwOIAYzCyqfIYEQZWXsaeDpBKajq_E-3oIEnkYU/edit"
+             }
+          ]
+       },
+       "type":"knowledgectr",
+       "heading":"Knowledge Center",
+       "slide_number":3
+    },
+    "4":{
+       "body":{
+          "map":"<div><iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.1693571231563!2d-79.39719274890832!3d43.64464467901919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34d97247bbc3%3A0xbc1d36bccb4b45ba!2sLighthouse+Labs!5e0!3m2!1sen!2sca!4v1463603588516\" frameborder=\"0\" allowfullscreen></iframe><div><span class=\"fa fa-building\"></span> 46 Spadina Ave, Toronto, ON <a href=\"#\">Get Directions</a></div><div><span class=\"fa fa-user\"></span> Office Manager - <a href=\"#\">John Smith</a></div><div><span class=\"fa fa-envelope\"></span><a href=\"#\">J.Smith@lighthouselabs.ca</a></div><div><div class=\"uber-promo\"> Complimentary UBER Code <strong>Welcome2LighthouseLabs</strong></div></div></div>",
+          "agenda":[
+             {
+                "desc":"Morning Class (2.5 hours)",
+                "time":"9:00am - 10:30am"
+             },
+             {
+                "desc":"Sinatra Apps (Long)",
+                "time":"10:30am - End of Day"
+             }
+          ]
+       },
+       "type":"day1agenda",
+       "heading":"Your first day - February 29th, 2016",
+       "slide_number":4
+    }
+  }'
 );
 
-INSERT INTO users (id, username, password, is_admin, first_name, last_name, personal_email, company_id, role_id) VALUES
+INSERT INTO users (id, username, password, is_admin, first_name, last_name, personal_email, company_id, role_id, profile_img) VALUES
 
-  ('a4bd224f-9aa6-4f15-b3c9-cb7551cd797f', 'rn@qrtrmstr.com', crypt('password', gen_salt('bf')), true, 'Rohan', 'Nair', 'r@rohannair.ca', 'nGLHsVI', 1),
-  ('a24b4195-4a49-450b-9b30-81632ef4c245', 'usersname2@email.com', crypt('password', gen_salt('bf')), false, 'Ron', 'Swanson', 'rs@parks.rec', 'nGLHsVI', 2),
-  ('2bd3d5b7-e013-40af-b236-770844e55124', 'usersname3@email.com', crypt('password', gen_salt('bf')), false, 'Lesley', 'Knope', 'lk@parks.rec', 'nGLHsVI', 3),
-  ('376e0300-22d4-4d66-ad31-caf91882964d', 'kate@lighthouselabs.ca', crypt('kate123', gen_salt('bf')), true, 'Kate', 'Ciborowski', 'kate@lighthouselabs.ca', 'OeUMkxw', 5),
-  ('4f0eefc6-dbee-4974-bacb-9f02c112718e', 'jeremy@lighthouselabs.ca', crypt('jeremy123', gen_salt('bf')), false, 'Jeremy', 'Shaki', 'jeremy@lighthouselabs.ca', 'OeUMkxw', 5);
+  ('a4bd224f-9aa6-4f15-b3c9-cb7551cd797f', 'rn@qrtrmstr.com', crypt('password', gen_salt('bf')), true, 'Rohan', 'Nair', 'r@rohannair.ca', 'nGLHsVI', 1, 'https://s3.amazonaws.com/qrtrmstr-images/kitten_face.png'),
+  ('a24b4195-4a49-450b-9b30-81632ef4c245', 'usersname2@email.com', crypt('password', gen_salt('bf')), false, 'Ron', 'Swanson', 'rs@parks.rec', 'nGLHsVI', 2, null),
+  ('2bd3d5b7-e013-40af-b236-770844e55124', 'usersname3@email.com', crypt('password', gen_salt('bf')), false, 'Lesley', 'Knope', 'lk@parks.rec', 'nGLHsVI', 3, null),
+  ('376e0300-22d4-4d66-ad31-caf91882964d', 'kate@lighthouselabs.ca', crypt('kate123', gen_salt('bf')), true, 'Kate', 'Ciborowski', 'kate@lighthouselabs.ca', 'OeUMkxw', 5, 'https://s3.amazonaws.com/qrtrmstr-images/kitten_face.png'),
+  ('4f0eefc6-dbee-4974-bacb-9f02c112718e', 'jeremy@lighthouselabs.ca', crypt('jeremy123', gen_salt('bf')), false, 'Jeremy', 'Shaki', 'jeremy@lighthouselabs.ca', 'OeUMkxw', 5, null);
