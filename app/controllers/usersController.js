44339873004eb56f72f733974bc0a50cd5e2766c
Stosh Fabricius <stoshfabricius@users.noreply.github.com>
2016-05-31 14:42:20 -0400

    DEV-75 Change DELETE route auth to use tokens (#31)
    * Replace database call with a token check in DELETE routes
    
    * Remove console.log

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -73,11 +73,12 @@ const usersController = () => {
 
     DELETE: function* () {
       try {
-        const user = yield this.models.User.query().getUserById(this.request.body.userId);
-        if(user[0].is_admin) {
+        if(this.state.user.isAdmin) {
           const result = yield this.models.User.query().putUser({ deleted: true }, this.params.id);
           this.status = 201;
-          this.body = result;
+          this.body = {
+            message: 'User has been deleted'
+          };
         }
         else {
           throw 'Unauthorized user attempted to delete another user';
