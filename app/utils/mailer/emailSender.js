8c0057b78064e02a347f2af2914330c8d474d56c
Stosh Fabricius <stosh.fabricius@gmail.com>
2016-06-07 13:07:36 -0400

    Clean up

--- app/utils/mailer/emailSender.js
+++ app/utils/mailer/emailSender.js
@@ -4,10 +4,7 @@ const ApiError = require('./../customErrors');
 module.exports = function (EmailToSend) {
   return new Promise((resolve, reject) => {
     EmailClient.transmissions.send(EmailToSend, (err, res) => {
-      if (err)  {
-        console.log(err);
-        reject( new ApiError('Error sending email, please try again', 400, err) )
-      }
+      if (err)  reject( new ApiError('Error sending email, please try again', 400, err) );
       resolve();
     });
   });
