29dd5856ea8a795743f27834021fc2a176d7f5fb
Stosh Fabricius <stosh.fabricius@gmail.com>
2016-06-07 10:33:58 -0400

    Fix error handling in email sender

--- app/utils/mailer/emailSender.js
+++ app/utils/mailer/emailSender.js
@@ -1,9 +1,13 @@
 const EmailClient = require('./mailerConfig');
+const ApiError = require('./../customErrors');
 
 module.exports = function (EmailToSend) {
   return new Promise((resolve, reject) => {
     EmailClient.transmissions.send(EmailToSend, (err, res) => {
-      if (err)  { throw new ApiError('Error sending email, please try again', 400, 'danger', 'Email Failed to Send') }
+      if (err)  {
+        console.log(err);
+        reject( new ApiError('Error sending email, please try again', 400, err) )
+      }
       resolve();
     });
   });
