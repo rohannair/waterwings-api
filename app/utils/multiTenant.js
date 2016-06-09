d0e18112b204e7e2c8630cafb49b29298295f548
Stosh Fabricius <stoshfabricius@users.noreply.github.com>
2016-06-09 17:06:07 -0400

    Move fileService into main API (#45)

--- app/utils/multiTenant.js
+++ app/utils/multiTenant.js
@@ -1,9 +1,12 @@
 const ApiError = require('./customErrors');
 
-function* getSubdomain(url) {
+function getSubdomain(url) {
   // TODO: Need to check that the subdomin belongs to a authorized Company
-  if(url.split('.').length < 2) throw new ApiError('No subdomain was provided for request', 404, 'Request is missing subdomain');
-  return url.split('.')[0].toLowerCase();
+  return (new Promise((resolve, reject) => {
+    if(url.split('.').length < 2) reject(new ApiError('No subdomain was provided for request', 404, 'Request is missing subdomain'));
+    resolve(url.split('.')[0].toLowerCase());
+    })
+  )
 }
 
 module.exports = {
